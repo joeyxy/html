92e31fd6c5b25a5f0ebca2cc9dd67a031e1b6204
joey <joeyxy83@gmail.com>
2014-08-06 22:53:06 +0800

    edit user page

--- phpmysql3/9/edit_user.php
+++ phpmysql3/9/edit_user.php
@@ -23,54 +23,55 @@
    if(empty($_POST['first_name'])){
    $errors[] = 'You forgot to enter your first name.';
    }else{
-   $fn = mysqli_real_escape_string($dbc,trim($POST['first_name']));
+   $fn = mysqli_real_escape_string($dbc,trim($_POST['first_name']));
    }
 
    if(empty($_POST['last_name'])){
     $errors[] = 'You forgot to enter your last name.';
      }else{
-     $ln = mysqli_real_escape_string($dbc,trim($POST['last_name']));
+     $ln = mysqli_real_escape_string($dbc,trim($_POST['last_name']));
    }
 
     if(empty($_POST['email'])){
      $errors[] = 'You forgot to enter your email address.';
      }else{
-     $e = mysqli_real_escape_string($dbc,trim($POST['email']));
+     $e = mysqli_real_escape_string($dbc,trim($_POST['email']));
     }
 
  if(empty($errors)){
 
   $q = "select user_id FROM users where email='$e' and user_id != $id";
   $r = @mysqli_query($dbc,$q);
-  if (mysqli_num_rows($r) == 0){
+ if (mysqli_num_rows($r) == 0){
   $q = "update users set first_name='$fn',last_name='$ln',email='$e' where user_id=$id limit 1";
   $r = @mysqli_query($dbc,$q);
-if(mysqli_affected_rows($dbc) == 1){
+    if(mysqli_affected_rows($dbc) == 1){
 
    echo '<p>The user has been edited.</p>';
   } else {
     echo '<p class="error">The user could not be edited due to a system error.</p>';
     echo '<p>'.mysqli_error($dbc).'<br />Query: ' .$q.'</p>';
-  } else
+  }} else
   {
   echo '<p class="error"> The email address has already been registered.</p>';
   }
   }else{
    echo '<p class="error">The following error occurred:<br />';
-   foreach ($erros as $msg){
-
+   foreach ($errors as $msg){
+    echo " - $msg <br />\n";
    }
+   echo '</p><p>please try again.</p>';
   }
-  $q = "select concat(last_name,',',first_name) from users where user_id=$id";
+  }
+  $q = "select last_name,email,first_name from users where user_id=$id";
   $r = @mysqli_query($dbc,$q);
 
   if(mysqli_num_rows($r) == 1){
    $row= mysqli_fetch_array($r,MYSQLI_NUM);
-   echo '<form action="delete_user.php" method="post">
-   <h3>Name:'.$row[0].'</h3>
-   <p>Are you sure you want to delete this user?<br />
-   <input type="radio" name="sure" value="Yes" />Yes
-   <input type="radio" name="sure" value="No" checked="checked" />No </p>
+   echo '<form action="edit_user.php" method="post">
+   <p>First Name:<input type="text" name="first_name" size="15" maxlength="15" value="'.$row[2].'" /></p>
+   <p>Last Name:<input type="text" name="last_name" size="15" maxlength="35" value="'.$row[0].'" /></p>
+   <p>Email Address:<input type="text" name="email" size="25" maxlength="55" value="'.$row[1].'" /></p>
    <p><input type="submit" name="submit" value="Submit" /></P>
    <input type="hidden" name="submitted" value="True" />
    <input type="hidden" name="id" value="'.$id.'" />
